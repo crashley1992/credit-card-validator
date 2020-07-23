57d39467396a3deb762112e0dbe655e9c46bcc29
Ashley Clarke <crashley92@gmail.com>
2020-07-22 18:31:41 -0700

    close enough?

--- index.py
+++ index.py
@@ -1,9 +1,9 @@
-import sys
-
 # double every second digit
 def checkSum(numbers):
     # get length of numbers input
     length = len(numbers)
+    sum = 0
+
     # checks from the last digit in numbers input and checks every second number
     for i in range(length-2,0,-2):
         # gets the value of each number and not the index position
@@ -12,8 +12,11 @@ def checkSum(numbers):
              double_num = num * 2
             #  check for if the doubled number is greater and 9
              if double_num > 9:
-                 print(double_num)
-             
-        
-
-checkSum([2,9,3,8,5])
+                 sum += double_num - 9
+                 if sum % 10 == 0:
+                     print('card is valid')
+                 else:
+                     print('card is not valid')
+                 
+                 
+checkSum()
